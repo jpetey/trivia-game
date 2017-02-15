982aa5c49d1e73003b64a2db67116eddffd132da
jpetey <jeremee.peters@gmail.com>
2017-02-15 08:44:48 -0800

    JS Questions transitions and timer done.

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,13 +35,14 @@ var correctCount = 0;
 var incorrectCount = 0;
 var unansweredCount = 0;
 
-var seconds = 11;
+var seconds = 5;
 var roundTimer;
 
 // PLAY THE GAME!
 
 function displayStart() {
 	$("#start-btn").html("START");
+	$("#replay-btn").empty();
 }
 
 //... Countdown Timer...
@@ -59,36 +60,28 @@ function tickDown () {
 	if (seconds === 0) {
 		clearInterval(roundTimer);
 		$("#game-section-2").html("Time's Up!");
-		$("#game-section-3").html("The correct answer is " 
+		$("#game-section-3").html("The correct answer is: " 
 			+ trivia[round].options[trivia[round].correctAnswer] +
 			"<br>(IMG PLACEHOLDER)");
 		unansweredCount++;
-		setTimeout(nextRound,5000);
-	};
 
-	
-}
+		setTimeout(nextRound,5000);
+	}
+};	
+
 
 function startTrivia () {
 	//... Countdown Timer, Trivia Question #1 & 4 Answer Options
-	$("#game-section-1").html("Timer: 00:11");
-	$("#game-section-2").html(trivia[round].question);
-	$("#choice-1").html(trivia[round].options[0]);
-	$("#choice-2").html(trivia[round].options[1]);
-	$("#choice-3").html(trivia[round].options[2]);
-	$("#choice-4").html(trivia[round].options[3]);
+	seconds = 5;
 
-	roundTimer = setInterval(tickDown,1000);
-
-};
-
-function nextRound () {
-	seconds = 11;
-	round++;
+	correctCount = 0;
+	incorrectCount = 0;
+	unansweredCount = 0;
+	round = 0;
+	$("#replay-btn").empty();
 	$("#game-section-1").html("Timer: 00:11");
 	$("#game-section-2").html(trivia[round].question);
 	$("#game-section-3").empty();
-	// Create <div> containers for each of the 4 choices
 	for (var i = 0; i < 4; i++) {
         var choiceDiv = $("<div>");  
       // Add class='choices' for styling
@@ -99,10 +92,47 @@ function nextRound () {
         choiceDiv.text(trivia[round].options[i]);
       // Append text to "game-section-3" <div>
         $("#game-section-3").append(choiceDiv);
-     }
+    }
 
 	roundTimer = setInterval(tickDown,1000);
+
 };
+
+function nextRound () {
+	console.log("Round # just before increase: " + round);
+	round++;
+	console.log("Round # just after increase: " + round);
+
+	if (round < trivia.length) {
+		seconds = 5;
+		$("#game-section-1").html("Timer: 00:30");
+		$("#game-section-2").html(trivia[round].question);
+		$("#game-section-3").empty();
+		// Create <div> containers for each of the 4 choices
+		for (var i = 0; i < 4; i++) {
+	        var choiceDiv = $("<div>");  
+	      // Add class='choices' for styling
+	        choiceDiv.addClass("choices");
+	      // Give each <div> a data-attribute for ID'ing
+	        choiceDiv.attr("data-option", (i + 1));
+	      // Populate each <div> with text
+	        choiceDiv.text(trivia[round].options[i]);
+	      // Append text to "game-section-3" <div>
+	        $("#game-section-3").append(choiceDiv);
+	    }
+
+		roundTimer = setInterval(tickDown,1000);
+
+	} else if (round >= trivia.length) {
+
+		$("#game-section-2").html("All Done! Here's how you did...");
+		$("#game-section-3").html("<p>Correct Answers: " + correctCount + "</p>"
+			+ "<p>Incorrect Answers: " + incorrectCount + "</p>"
+			+ "<p>Unanswered: " + unansweredCount + "</p>");
+		$("#replay-btn").html("Play Again!");
+	}
+};
+
 
 	// If timer reaches 0, do the following for 5 seconds...
 
@@ -153,18 +183,11 @@ function nextRound () {
 displayStart();
 
 // Initialize game when user clicks 'Start' Button
-// Replace Start Button with Time + Q&As
-// Start timer countdown from :30
 $("#start-btn").click(startTrivia);
 
-// If timer reaches 0, do the following for 5 seconds...
+// Record Click on Answer Choice
 
-	// ... Freeze timer (at 0)
 
-	// ... Replace Q/A section with...
+// Replay game
+$("#replay-btn").click(startTrivia);
 
-		// ... message = "Time's Up"
-
-		// ... message = "The Correct Answer Is: ___"
-
-		// ... (optional) image/gif assoc with correct answer
