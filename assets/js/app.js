01688f74fff282a04b629def093da1e033ed3da6
jpetey <jeremee.peters@gmail.com>
2017-02-15 17:08:33 -0800

    Final Game. Styling, HTML, & JS done.

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,26 +5,62 @@ var trivia = [
 	question: "Who was the first President of Zimbabwe?",
 	options: ["President Banana", "President Camacho", "President Ka-blam-o", "President Burrito"],
 	correctAnswer: 0,
+	media: "assets/images/banana.jpg",
 	},
 	{
 	question: "What is the national animal of Scotland?",
 	options: ["Beaver", "Mountain Goat", "Black Adder", "Unicorn"],
 	correctAnswer: 3,
+	media: "assets/images/unicorn.gif",
 	},
 	{
 	question: "Before Nirvana was 'Nirvana', they went by several other band names. Which band name is NOT one of those precursory names?",
 	options: ["Dead Freds", "Skid Row", "Bliss", "Fecal Matter"],
 	correctAnswer: 0,
+	media: "assets/images/nirvana.jpg",
 	},
 	{
-	question: "This current Major League Baseball manager & former star player is credited with inventing the high-five, along with Dodgers teammate Glenn Burke, on October 2, 1977.",
+	question: "Who is the only U.S. President to ever be granted a patent?",
+	options: ["Thomas Jefferson", "Abraham Lincoln", "Franklin D. Roosevelt", "Benjamin Harrison"],
+	correctAnswer: 1,
+	media: "assets/images/abe.gif",
+	},
+	{
+	question: "Which country boasts the oldest, continually used national flag (which is still in use today)?",
+	options: ["Russia", "Turkey", "Brazil", "Denmark"],
+	correctAnswer: 3,
+	media: "assets/images/denmark.gif",
+	funFact: "The current design of a white Scandinavian cross on a red back ground was adopted in 1625 and its square shape in 1748."
+	},
+	{
+	question: "What was the first product to ever have a barcode?",
+	options: ["Heinz Ketchup", "Kellog's Corn Flakes", "Wrigley Chewing Gum", "Ritz Crackers"],
+	correctAnswer: 2,
+	media: "assets/images/wrigley.jpg",
+	},
+	{
+	question: "What popular American food brand accidentally translated their popular slogan to 'eat your fingers off' when first opening for business in China?",
+	options: ["Campbell's Soup", "Lay's Potato Chips", "Kentucky Fried Chicken", "Applebee's"],
+	correctAnswer: 2,
+	media: "assets/images/kfc.gif",
+	},
+	{
+	question: "From 1859 - 1994, what sport was the official 'National Game of Canada', before hockey was added as a co-sport in 1994?",
+	options: ["Rugby", "Ice Fishing", "Beaver Trapping", "Lacrosse"],
+	correctAnswer: 3,
+	media: "assets/images/lax.gif",
+	},	
+	{
+	question: "Which current Major League Baseball manager and former star player is credited with inventing the high-five, along with Dodgers teammate Glenn Burke?",
 	options: ["Bud Black", "Dusty Baker", "Joe Maddon", "Don Mattingly"],
 	correctAnswer: 1,
+	media: "assets/images/h5.gif",
 	},
 	{
-	question: "A 'Death Spiral' is created when THIS creature accidentally misinterprets chemical trails left by its own species & starts walking in circles. If too many join in, it can kill all involved.",
+	question: "A'Death Spiral' is created when THIS creature accidentally misinterprets chemical trails left by its own species and starts walking in circles. If too many join in, it can kill all involved.",
 	options: ["Lemmings", "Chinchillas", "Ants", "Termites"],
 	correctAnswer: 2,
+	media: "assets/images/ants.gif",
 	},
 
 ]
@@ -35,14 +71,14 @@ var correctCount = 0;
 var incorrectCount = 0;
 var unansweredCount = 0;
 
-var seconds = 5;
+var seconds = 30;
 var roundTimer;
 var userChoice;
 
 // PLAY THE GAME!
 
 function displayStart() {
-	$("#start-btn").html("START");
+	$("#start-btn").html("<img src='assets/images/door.jpg'>");
 }
 
 //... Countdown Timer...
@@ -59,10 +95,10 @@ function tickDown () {
 	
 	if (seconds === 0) {
 		clearInterval(roundTimer);
-		$("#game-section-2").html("Time's Up!");
+		$("#game-section-2").html("TIME'S UP!");
 		$("#game-section-3").html("The correct answer is: " 
 			+ trivia[round].options[trivia[round].correctAnswer] +
-			"<br>(IMG PLACEHOLDER)");
+			"<br><br><img src=" + trivia[round].media + " height='375px'>");
 		unansweredCount++;
 
 		setTimeout(nextRound,5000);
@@ -72,7 +108,7 @@ function tickDown () {
 
 function startTrivia () {
 	//... Countdown Timer, Trivia Question #1 & 4 Answer Options
-	seconds = 5;
+	seconds = 30;
 
 	correctCount = 0;
 	incorrectCount = 0;
@@ -96,6 +132,32 @@ function startTrivia () {
 
 	roundTimer = setInterval(tickDown,1000);
 
+    $(".choices").on("click", function() {
+	console.log(parseInt($(this).data("option")));
+	userChoice = parseInt($(this).data("option"));
+
+		if (userChoice === trivia[round].correctAnswer) {
+			correctCount++;
+			clearInterval(roundTimer);
+			$("#game-section-2").html("CORRECT!");
+			$("#game-section-3").html("The correct answer is: " 
+			+ trivia[round].options[trivia[round].correctAnswer] +
+			"<br><br><img src=" + trivia[round].media + " height='375px'>");
+
+			setTimeout(nextRound,5000);
+
+		} else if (userChoice !== trivia[round].correctAnswer) {   
+			incorrectCount++;
+			clearInterval(roundTimer);
+			$("#game-section-2").html("INCORRECT!");
+			$("#game-section-3").html("The correct answer is: " 
+			+ trivia[round].options[trivia[round].correctAnswer] +
+			"<br><br><img src=" + trivia[round].media + " height='375px'>");
+
+		setTimeout(nextRound,5000);		
+		}
+	});
+
 };
 
 function nextRound () {
@@ -104,7 +166,7 @@ function nextRound () {
 	// console.log("Round # just after increase: " + round);
 
 	if (round < trivia.length) {
-		seconds = 5;
+		seconds = 30;
 		$("#game-section-1").html("Timer: 00:30");
 		$("#game-section-2").html(trivia[round].question);
 		$("#game-section-3").empty();
@@ -134,17 +196,17 @@ function nextRound () {
 				$("#game-section-2").html("Correct!");
 				$("#game-section-3").html("The correct answer is: " 
 				+ trivia[round].options[trivia[round].correctAnswer] +
-				"<br>(IMG PLACEHOLDER)");
+				"<br><br><img src=" + trivia[round].media + " height='375px'>");
 
 				setTimeout(nextRound,5000);
 
 			} else if (userChoice !== trivia[round].correctAnswer) {   
 				incorrectCount++;
 				clearInterval(roundTimer);
-				$("#game-section-2").html("Incorrect!");
+				$("#game-section-2").html("INCORRECT!");
 				$("#game-section-3").html("The correct answer is: " 
 				+ trivia[round].options[trivia[round].correctAnswer] +
-				"<br>(IMG PLACEHOLDER)");
+				"<br><br><img src=" + trivia[round].media + " height='375px'>");
 
 			setTimeout(nextRound,5000);		
 			}
@@ -164,6 +226,15 @@ function nextRound () {
 
 
 };
+
+displayStart();
+
+// Initialize game when user clicks 'Start' Button
+$("#start-btn").click(startTrivia);
+
+
+///---my notes--//
+
 
 
 	// If timer reaches 0, do the following for 5 seconds...
@@ -212,9 +283,3 @@ function nextRound () {
 //----------
 
 // Start with 'Start' Button displayed
-displayStart();
-
-// Initialize game when user clicks 'Start' Button
-$("#start-btn").click(startTrivia);
-
-// Record Click on Answer Choice
